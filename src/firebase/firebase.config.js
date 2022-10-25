bc77d2cd71fa4e52cfcc1751fc6b15d557952f1c
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-25 18:54:58 +0600

    Starting Project with Website title & favicon

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTwaW00KpjDIT3TnGdPzWtI1DjB87v3qQ",
+  authDomain: "freelancing-educare-institute.firebaseapp.com",
+  projectId: "freelancing-educare-institute",
+  storageBucket: "freelancing-educare-institute.appspot.com",
+  messagingSenderId: "63258713891",
+  appId: "1:63258713891:web:9e55edd210d73224a23696",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

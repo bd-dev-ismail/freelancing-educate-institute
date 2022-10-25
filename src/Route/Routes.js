721acfed17d7a66085787218c0d656e82a5a8bc7
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-25 19:38:17 +0600

    responsive navbar creating

---
+++ src/Route/Routes.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Blog from "../components/Blog/Blog";
+import Courses from "../components/Courses/Courses";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Faq from "../components/FAQ/Faq";
+import Home from "../components/Home/Home";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+    {path: '/', element: <Main></Main>, errorElement: <ErrorPage></ErrorPage>, children: [
+        {
+            path: '/', element: <Home></Home>
+        },
+        {
+            path: '/courses', element: <Courses></Courses>
+        },
+        {
+            path: '/faq', element: <Faq></Faq>
+        },
+        {
+            path: '/blog', element: <Blog></Blog>
+        }
+    ]}
+])
